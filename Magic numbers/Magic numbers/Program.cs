5f0db99e79638789d765bf1a89ae5cdbdd76f205
Kagatun <KAGATUN777@MAIL.RU>
2025-01-22 16:02:54 +0300

    1
    1

---
+++ Magic numbers/Magic numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿class Weapon
+{
+    private int _bulletsCount;
+
+    public bool CanShoot => _bulletsCount > 0;
+
+    public void Shoot()
+    {
+        if(CanShoot == false)
+            throw new InvalidOperationException();
+
+        _bulletsCount --;
+    }
+}
