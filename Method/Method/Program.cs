a83dd8ef9435653c79aa219b4eb2aeff9e458aa5
Kagatun <KAGATUN777@MAIL.RU>
2025-01-22 20:32:48 +0300

    1
    1

---
+++ Method/Method/Program.cs
@@ -0,0 +1,12 @@
+﻿class Task
+{
+    public static int ClampValue(int value, int minValue, int maxValue)
+    {
+        if (value < minValue)
+            return minValue;
+        else if (value > maxValue)
+            return maxValue;
+        else
+            return value;
+    }
+}
