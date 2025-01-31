24c51800ef02a7c855d8aeabd0d8e3d9603eebf1
Kagatun <KAGATUN777@MAIL.RU>
2025-01-31 16:05:26 +0300

    1
    1

--- C#/C#/Program.cs
+++ C#/C#/Program.cs
@@ -1,19 +1,22 @@
 ﻿namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int hitPointHero;
-            byte numberBullets;
-            string greeting;
-            ushort parkingPlaces;
-            float startOfMovement;
-            short bankAccount;
-            uint planetAge;
-            ulong starAge;
-            double jumpHeight;
-            bool isDead;
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Сколько Вам лет?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Ваш знак зодиака?");
+            string zodiac = Console.ReadLine();
+
+            Console.WriteLine("Где вы работаете?");
+            string work = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут " + name + ", Вам " + age + " лет," + " Ваш знак зодиака " + zodiac + ", Вы работаете на(в) " + work + ".");
         }
     }
 }
