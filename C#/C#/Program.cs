25da8584a76bb4d5a9c3fb76052d52b446955d94
Kagatun <KAGATUN777@MAIL.RU>
2025-01-31 17:19:07 +0300

    1
    1

--- C#/C#/Program.cs
+++ C#/C#/Program.cs
@@ -5,20 +5,16 @@
         static void Main(string[] args)
         {
             int waitingTime = 10;
-            int totalMinutes;
             int minutesPerHour = 60;
-            int hoursOfWaiting;
-            int minutesOfWaiting;
-            int numberOfPatients;
 
             Console.Write("Введите колличество пациентов:");
-            numberOfPatients = Convert.ToInt32(Console.ReadLine());
+            int numberPatients = Convert.ToInt32(Console.ReadLine());
 
-            totalMinutes = (numberOfPatients * waitingTime);
-            hoursOfWaiting = (totalMinutes / minutesPerHour);
-            minutesOfWaiting = (totalMinutes % minutesPerHour);
+            int totalMinutes = (numberPatients * waitingTime);
+            int waitingHours = (totalMinutes / minutesPerHour);
+            int waitingMinutes = (totalMinutes % minutesPerHour);
 
-            Console.WriteLine("Вам осталось ждать до своей очереди " + hoursOfWaiting + " часов и " + minutesOfWaiting + " минут.");
+            Console.WriteLine("Вам осталось ждать до своей очереди " + waitingHours + " часов и " + waitingMinutes + " минут.");
         }
     }
 }
