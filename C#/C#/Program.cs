eb8d833d566e2e3f21a2f5a08f232a6073f52752
Kagatun <KAGATUN777@MAIL.RU>
2025-01-31 18:45:43 +0300

    1
    1

--- C#/C#/Program.cs
+++ C#/C#/Program.cs
@@ -1,21 +1,14 @@
-﻿namespace ConsoleApp5
+﻿class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int waitingTime = 10;
-            int minutesPerHour = 60;
+        Console.WriteLine("Введите имя:");
+        string name = Console.ReadLine();
 
-            Console.Write("Введите колличество пациентов:");
-            int numberPatients = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число сколько раз нужно повторить имя?");
+        int numberRepetitions = Convert.ToInt32(Console.ReadLine());
 
-            int totalMinutes = (numberPatients * waitingTime);
-            int waitingHours = (totalMinutes / minutesPerHour);
-            int waitingMinutes = (totalMinutes % minutesPerHour);
-
-            Console.WriteLine("Вам осталось ждать до своей очереди " + waitingHours + " часов и " + waitingMinutes + " минут.");
-        }
+        for (int i = 0; i < numberRepetitions; i++)
+            Console.WriteLine(name);
     }
 }
-
